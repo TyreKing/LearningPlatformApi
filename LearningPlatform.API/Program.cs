fbad7357d8c013d5a8853da913b16daed951a42c
Anatoly Sergov <Anatolysergov@gmail.com>
2024-03-22 17:46:33 -0400

    setup db config and initial db setup

--- LearningPlatform.API/Program.cs
+++ LearningPlatform.API/Program.cs
@@ -1,23 +1,31 @@
+using LearningPlatform.API;
+using LearningPlatform.Context;
+
 var builder = WebApplication.CreateBuilder(args);
+{
+    builder.Services
+        .AddPresentation()
+        .AddContext(builder.Configuration);
+}
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+{
+    app.UseHttpsRedirection();
+    app.UseAuthentication();
+    app.MapControllers();
+    app.Run();
+}
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-app.UseAuthentication();
-app.MapControllers();
 
-app.Run();
 
